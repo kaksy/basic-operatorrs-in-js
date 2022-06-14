d42fa408ffe4b5960cfa96861a96a1e2d47c6cce
unknown <nsikakatan@gmail.com>
2022-06-14 00:35:27 -0700

    putting basic operators into practice

---
+++ script.js
@@ -0,0 +1,40 @@
+// let num =2,
+// value = -7,
+// student = num + value;
+// alert(student)
+console.log( 5 % 2);
+// exponentials
+console.log(2**2);
+console.log(2**3);
+console.log(4**1);
+console.log(4 ** (1/2));
+
+
+// using strings concatenation
+let q = 'woman' + 'man'
+console.log(typeof q)
+
+// when any operand is a String
+alert( '7'+ 3);
+
+
+// when 2 numbers come before a string it adds
+//  first b4 getting concanated into a string
+alert( 2 + 5 +'8' + 4)
+
+
+// but when a string comes first it concanates 
+// all os them e.g
+alert('5' + 2 + 4 + 7)
+// this is interpreted as '5'+2='52' 
+// then '52'+4= '524'
+
+// binary operator as only operator that supports stings others converts back to numbers
+let a = '16' - 4  -7 - 6 
+console.log(a)
+let num = 16
+a = '2'
+c = num % a
+console.log(c)
+
+console.log()
